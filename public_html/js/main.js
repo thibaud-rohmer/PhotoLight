27ae0cf920afe692dd1f48836a489a7a37035c06
thibaud.rohmer@gmail.com <thibaud.rohmer@gmail.com>
2012-11-20 14:00:10 +0100

    Nicer js

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,6 +1,15 @@
+function update_url(url,name){
+	if(typeof history.pushState == 'function') { 
+		var stateObj = { foo: "bar" };
+		history.pushState(stateObj, "PhotoLight - " + name, url);
+	}
+}
+
 $("document").ready(function(){
 	$(".folder").click(function(){
 		t=encodeURI($(this).children(".title").children('a').attr('href'));
+		n=$(this).children(".title").children('a').html();
 		$("body").load(t);
+		update_url(t,n);
 	})
 });
